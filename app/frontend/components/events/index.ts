8ed369da8c01f31e9481c74804d4b8f2766c9c7e
“Peteroche” <petergoddey08l@gmail.com>
2026-03-28 07:12:27 +0100

    feat: add EventRecommendationCarousel and its props to events index

--- app/frontend/components/events/index.ts
+++ app/frontend/components/events/index.ts
@@ -1,3 +1,5 @@
 export { EventDataProvider, EventListProvider } from './EventDataProvider';
 export { useEventData, useEventList } from './EventDataContext';
 export type { EventDataContextValue, EventListContextValue } from './EventDataContext';
+export { EventRecommendationCarousel } from './EventRecommendationCarousel';
+export type { EventRecommendationCarouselProps } from './EventRecommendationCarousel';
